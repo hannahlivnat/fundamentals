1dad6e85d15634ed054f4e7b6970d033e1bbfee7
Hannah L <hannahlivnat@gmail.com>
2020-04-14 13:29:38 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,18 @@
 console.log("Up and running!");
 
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
+const cards = ["queen", "queen", "king", "king"];
+const cardsInPlay = [];
+let cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log(`User flipped ${cardOne}`);
+let cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+console.log(`User flipped ${cardTwo}`);
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        alert("You found a match!");
+    } else {
+        alert("Sorry, try again.");
+    }
+} 
