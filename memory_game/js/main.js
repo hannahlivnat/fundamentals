16b50fc21cbcf468638a0bb43dbb39b0e0f5bb45
Hannah L <hannahlivnat@gmail.com>
2020-04-15 10:13:45 -0700

    two functions added, need to cont. working on checkForMatch

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,17 +2,27 @@ console.log("Up and running!");
 
 const cards = ["queen", "queen", "king", "king"];
 const cardsInPlay = [];
-let cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log(`User flipped ${cardOne}`);
-let cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-console.log(`User flipped ${cardTwo}`);
 
-if (cardsInPlay.length === 2) {
-    if (cardsInPlay[0] === cardsInPlay[1]) {
-        alert("You found a match!");
-    } else {
-        alert("Sorry, try again.");
-    }
-} 
+//function group together logic to check to see if two cards that the user flipped over match. 
+
+function checkForMatch () {
+    if (cardsInPlay.length === 2) {
+        if (cardsInPlay[0] === cardsInPlay[1]) {
+            alert("You found a match!");
+        } else {
+            alert("Sorry, try again.");
+        }
+    } 
+}
+
+//function storing all steps related to selecting, or flipping over, a card
+function flipCard (cardId) {
+    console.log(`User flipped ${cards[cardId]}.`)
+    cardsInPlay.push(cards[cardId]);
+    checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
+
+
