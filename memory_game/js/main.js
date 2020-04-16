8743a976a9e147df9471c1473f8bef2ab7d422e8
Hannah L <hannahlivnat@gmail.com>
2020-04-16 11:12:04 -0700

    created object for cards with image and suite

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,27 @@
 console.log("Up and running!");
 
-const cards = ["queen", "queen", "king", "king"];
+const cards = [
+    {
+        rank: "queen", 
+        suite: "hearts",
+        cardImage: "images/queen-of-hearts.png"
+    },
+    {
+        rank: "queen",
+        suite: "diamonds",
+        cardImage: "images/queen-of-diamonds.png"
+    },
+    {
+        rank: "king",
+        suite: "hearts",
+        cardImage: "images/king-of-hearts.png"
+    },
+    {
+        rank: "king",
+        suite: "diamonds",
+        cardImage: "images/king-of-diamonds.png"
+    }
+];
 const cardsInPlay = [];
 
 //function group together logic to check to see if two cards that the user flipped over match. 
@@ -17,8 +38,10 @@ function checkForMatch () {
 
 //function storing all steps related to selecting, or flipping over, a card
 function flipCard (cardId) {
-    console.log(`User flipped ${cards[cardId]}.`)
-    cardsInPlay.push(cards[cardId]);
+    console.log(`User flipped ${cards[cardId].rank}.`);
+    console.log(`User flipped ${cards[cardId].suite}.`);
+    console.log(`User flipps ${cards[cardId].cardImage}.`);
+    cardsInPlay.push(cards[cardId].rank);
     checkForMatch();
 }
 
